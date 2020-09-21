8d0339e368d679f8ae85bfb0c5609c7146fae324
Chabib Nurochman <chabib.nr@gmail.com>
2020-09-21 14:30:23 +0700

    Add support for ssr

--- dist/extend-reactive.js
+++ dist/extend-reactive.js
@@ -1,22 +1,34 @@
-export default extend;
-export function extend(object, extension) {
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.extend = void 0;
+exports.default = extend;
+function extend(object, extension) {
     return new Proxy(object, {
-        get(...args) {
-            const [, prop] = args;
+        get: function () {
+            var args = [];
+            for (var _i = 0; _i < arguments.length; _i++) {
+                args[_i] = arguments[_i];
+            }
+            var prop = args[1];
             if (prop in extension) {
                 return extension[prop];
             }
-            return Reflect.get(...args);
+            return Reflect.get.apply(Reflect, args);
         },
-        set(...args) {
-            const [, prop, value] = args;
+        set: function () {
+            var args = [];
+            for (var _i = 0; _i < arguments.length; _i++) {
+                args[_i] = arguments[_i];
+            }
+            var prop = args[1], value = args[2];
             if (prop in extension) {
                 ;
                 extension[prop] = value;
                 return true;
             }
-            Reflect.set(...args);
+            Reflect.set.apply(Reflect, args);
             return true;
         },
     });
 }
+exports.extend = extend;
